c0456d05c1c00854e4a6344fbff1e310997d029e
Quinn Banet <quinnbanet359@gmail.com>
2016-12-07 11:14:08 -0500

    Updated FireBase DB, so updated Local to match
    “teams” to “Teams”

--- app/src/main/java/com/quinnbanet/sustaingame/CompletedChallenge.java
+++ app/src/main/java/com/quinnbanet/sustaingame/CompletedChallenge.java
@@ -1,13 +1,18 @@
 package com.quinnbanet.sustaingame;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -16,7 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class CompletedChallenge extends Fragment {
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference mRef = firebaseDatabase.getReference().child("teams");
+    DatabaseReference mRef = firebaseDatabase.getReference().child("Teams");
 
     public CompletedChallenge() {
         //required empty constructor
@@ -34,7 +39,22 @@ public class CompletedChallenge extends Fragment {
                 tv.setText(model.getName());
             }
         };
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                //send user to new activity upon challenge click
+               // Toast toast = Toast.makeText(this, (String)arg0.getItemAtPosition(position), Toast.LENGTH_SHORT);
+                //toast.show();
+
+                //Snackbar.make(R.layout.activity_auth_dashboard, "Had a snack at Snackbar", Snackbar.LENGTH_LONG)
+                  //      .setAction("Undo", mOnClickListener)
+                   //     .setActionTextColor(Color.RED)
+                    //    .show();
+            }
+        });
         lv.setAdapter(la);
         return view;
     }
+
 }
